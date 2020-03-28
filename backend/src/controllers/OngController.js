5784452209046cb9ec033aa01e872574d0b1708b
MvP Mark <mvp.paulo@gmail.com>
2020-03-27 20:32:14 -0400

    Ajustes avançados

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,6 @@
 const connection = require('../database/connection');
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
+
 
 module.exports = {
 
@@ -7,8 +8,7 @@ module.exports = {
         const {name, email, whatsapp, city, uf} = request.body;
         // console.log(data);
         
-        const id = crypto.randomBytes(4).toString('HEX');
-    
+    const id = generateUniqueId();
         await connection('ongs').insert({
             id,
             name,
